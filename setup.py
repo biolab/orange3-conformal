057985204a4764b19722f77d67d0f7aa566a5e09
Tomaz Hocevar <tomaz.hocevar@fri.uni-lj.si>
2018-06-26 11:16:49 +0200

    Add license file to distribution

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 NAME = "Orange3-Conformal"
 
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 
 DESCRIPTION = "Orange3 Conformal Prediction library"
 # pandoc --from=markdown --to=rst README.md -o README.rst
